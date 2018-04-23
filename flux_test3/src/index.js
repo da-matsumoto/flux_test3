224a525ae1f34a5e813c9335ca08060e5fd356ec
da-matsumoto <da.mats1990@gmail.com>
2018-04-23 14:57:35 +0900

    fluxでstore管理

--- flux_test3/src/index.js
+++ flux_test3/src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Actions} from './action.js'
+import {nameStore, messageStore} from './stores.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class AppView extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {name: '', message: ''}
+        nameStore.onChange = () => {
+            this.setState({name: nameStore.name})
+        }
+        messageStore.onChange = () => {
+            this.setState({message: messageStore.message})
+        }
+    }
+    render () {
+        console.log('view.render')
+        return (<div>
+            <div>
+                <input value={this.state.name} onChange={(e) => Actions.changeName(e.target.value)} />
+                <button onClick={(e) => Actions.submitName()}>登録</button>
+            </div>
+            <div>{this.state.message}</div>
+        </div>)
+    }
+}
+
+ReactDOM.render(
+    <AppView />,
+    document.getElementById('root')
+)
---
+++ flux_test3/src/stores.js
@@ -0,0 +1,18 @@
+import {appDispatcher} from './appDispatcher.js'
+import {ActionType} from './action.js'
+
+export const nameStore = {name: '', onChange: null}
+export const messageStore = {message: '', onChange: null}
+
+appDispatcher.register(payload => {
+    if (payload.actionType === ActionType.CHANGE_NAME) {
+        nameStore.name = payload.value
+        nameStore.onChange()
+    }
+})
+appDispatcher.register(payload => {
+    if (payload.actionType === ActionType.SUBMIT_NAME) {
+        nameStore.message = nameStore.name + 'さん、こんにちは。'
+        messageStore.onChange()
+    }
+})
